d07194d1d92c87d2f5a306a346c1d37c2146b8ef
M4yfl0w3r <mr.mayflower15@gmail.com>
2022-04-29 16:33:06 +0200

    update .gitignore

--- Ex1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hope/Code/practise/novomatic/Ex1
-# Build directory: /home/hope/Code/practise/novomatic/Ex1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("build")
-subdirs("test")
--- Ex1/build/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/hope/Code/practise/novomatic/Ex1/build/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
